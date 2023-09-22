571ee5baad0294e2b1f5af88172469703800c7e2
Shivam Pande <shivshivampande18@gmail.com>
2023-09-22 11:35:25 +0530

    Create ConcatDLL.c

---
+++ DS LAB/Lab8/ConcatDLL.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    struct node* llink;
+    int data;
+    struct node* rlink;
+} node;
+
+node* x1 = NULL;
+node* x2 = NULL;
+
+node * insert(node* first, int val)
+{
+    node* cur;
+    node* temp = (node*) malloc(sizeof(node));
+    temp->data = val;
+    temp->rlink = NULL;
+    temp->llink = NULL;
+
+    if(first==NULL)
+    {
+        first= temp;
+    }
+    else
+    {
+        for(cur=first;cur->rlink!=NULL;cur=cur->rlink);
+
+        cur->rlink = temp;
+        temp->llink = cur;
+    }
+    return first;
+
+}
+
+void display(node* first)
+{
+    if(first == NULL) printf("Empty List:");
+    node* cur;
+
+    for(cur=first;cur!=NULL; cur = cur->rlink) printf("%d ", cur->data);
+    printf("\n");
+}
+
+int main()
+{
+    x1 = insert(x1,10);
+    x1 = insert(x1,20);
+    x1 = insert(x1,30);
+    display(x1);
+
+    x2 = insert(x2,100);
+    x2 = insert(x2,200);
+    x2 = insert(x2,300);
+    display(x2);
+
+
+    node* cur;
+    for(cur=x1;cur!=NULL;cur=cur->rlink);
+    cur->rlink = x2;
+    x2->llink = cur;
+    display(x1);
+    return 0;
+}
